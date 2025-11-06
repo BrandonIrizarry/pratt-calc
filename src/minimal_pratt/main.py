2c72f77942eb430b0878c9c7028ffa09a70b5eba
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-11-05 22:32:48 -0500

    docs: remove stream-of-consciousness comments

--- src/minimal_pratt/main.py
+++ src/minimal_pratt/main.py
@@ -54,18 +54,6 @@ def expression(tokens: list[Token], i: int, acc: int, level: int) -> tuple[int, 
     return acc, i
 
 
-# The goal is to first parse/evaluate single-number expressions.
-#
-# And so there are two kinds of token: one with a nud action of
-# evaluating to itself, and one with a led action of returning its
-# left argument only, and has the lowest binding precedence of all
-# tokens.
-
-# Using Precedence.EOF as the starting value of 'level' indicates that
-# this is the "primary" level, and by extension the "ground" level for
-# expressions.
-
-# then let's see how expressions with addition are handled.
 t2: list[Token] = [3, "+", 4, "eof"]
 
 value = expression(t2, 0, 0, Precedence.EOF)
