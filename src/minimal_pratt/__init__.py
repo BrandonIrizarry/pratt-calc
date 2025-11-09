dff00a6b6dc83d8dcd13d4e53ffc8188ebe4f9a6
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-11-08 22:49:24 -0500

    refactor: replace decorator with simpler 'run/main' distinction

--- src/minimal_pratt/__init__.py
+++ src/minimal_pratt/__init__.py
@@ -1,30 +1,14 @@
 import sys
-from collections.abc import Callable
 
 from minimal_pratt.parser import Parser
 from minimal_pratt.tokenizer import tokenize
 
 
-def _console(fn: Callable[[str], int]):
-    """A wrapper to handle console I/O administrivia."""
-
-    def wrapper(inp: str | None) -> int:
-        """Tests expect INP as a string.
-
-        Production expects INP to come from the console.
-
-        """
-
-        value = fn(sys.argv[1] if inp is None else inp)
-
-        print(value)
-
-        return value
-
-    return wrapper
+def run():
+    value = main(sys.argv[1])
+    print(value)
 
 
-@_console
 def main(raw_expression: str) -> int:
     """The proper entry-point into the application.
 
