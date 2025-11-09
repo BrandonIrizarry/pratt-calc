49eaca6a548531a115cd67a6ee6615639bf3f3a3
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-11-08 22:22:05 -0500

    feat: make main accept input as well
    Now, 'main' should be console-agnostic.

--- src/minimal_pratt/__init__.py
+++ src/minimal_pratt/__init__.py
@@ -5,18 +5,24 @@ from minimal_pratt.parser import Parser
 from minimal_pratt.tokenizer import tokenize
 
 
-def _display(fn: Callable[[], int]):
+def _console(fn: Callable[[str], int]):
+    """A wrapper to handle console I/O administrivia."""
+
     def wrapper():
-        value = fn()
+        value = fn(sys.argv[1])
 
         print(value)
 
     return wrapper
 
 
-@_display
-def main() -> int:
-    raw_expression = sys.argv[1]
+@_console
+def main(raw_expression: str) -> int:
+    """The proper entry-point into the application.
+
+    Consume RAW_EXPRESSION, and compute an integer result.
+
+    """
 
     stream = tokenize(raw_expression)
     parser = Parser(stream)
