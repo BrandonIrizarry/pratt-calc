b3669916edfe66eb52d00e13fd44c95bebb66a7f
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-11-08 22:41:37 -0500

    test: update tests to use expression literals instead of token lists

--- test/test_arith.py
+++ test/test_arith.py
@@ -1,25 +1,24 @@
 import pytest
 
-from minimal_pratt.main import Parser, Token
+from minimal_pratt import main
 
 examples = [
-    ([3, "eof"], 3),
-    ([0, "eof"], 0),
-    ([3, "+", 4, "eof"], 7),
-    ([3, "+", 4, "*", 5, "+", 6, "eof"], 29),
-    ([3, "+", "-", 4, "*", 5, "+", 6, "eof"], -11),
-    (["-", 3, "+", 4, "eof"], 1),
-    (["(", 3, "+", "-", 4, ")", "*", 5, "+", 6, "eof"], 1),
-    ([2, "^", 3, "^", 2, "eof"], 512),
-    ([2, "^", 3, "*", 3, "eof"], 24),
-    ([2, "^", "(", 3, "*", 2, ")", "eof"], 64),
-    ([2, "-", 3, "*", 2, "eof"], -4),
-    ([["-", "(", 3, "+", 1, ")", "!", "eof"], -24]),
+    ("3", 3),
+    ("3 + 4", 7),
+    ("3 + 4 * 5 + 6", 29),
+    ("3 + -4 * 5 + 6", -11),
+    ("-3 + 4", 1),
+    ("(3 + -4) * 5 + 6", 1),
+    ("2^3^2", 512),
+    ("2^3*3", 24),
+    ("2^(3*2)", 64),
+    ("2-3*2", -4),
+    ("-(3 + 1)!", -24),
 ]
 
 
-@pytest.mark.parametrize("tokens, value", examples)
-def test_examples(tokens: list[Token], value: int):
-    result = Parser(tokens).expression()
+@pytest.mark.parametrize("raw_expression, value", examples)
+def test_examples(raw_expression: str, value: int):
+    result = main(raw_expression)
 
     assert result == value
