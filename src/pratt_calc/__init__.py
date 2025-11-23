fd163fd9acb47554894000058d241150727c6fe8
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-11-23 11:14:24 -0500

    feat: launch REPL in the absence of FILENAME argument

--- src/pratt_calc/__init__.py
+++ src/pratt_calc/__init__.py
@@ -3,6 +3,7 @@ from typing import Annotated
 import typer
 
 from pratt_calc.main import evaluate
+from pratt_calc.repl import Repl
 
 
 def app():
@@ -11,11 +12,15 @@ def app():
     # Note that the name of this particular function is insigificant,
     # as the function only serves to wrap the logic used by
     # 'typer.run'
-    def cli(filename: Annotated[str, typer.Argument(help="Path to source file")]):
+    def cli(filename: Annotated[str, typer.Argument(help="Path to source file")] = ""):
         """Pratt Calc application."""
-        with open(filename, encoding="utf-8") as f:
-            code = f.read()
 
-            print(evaluate(code))
+        if filename == "":
+            Repl().cmdloop()
+        else:
+            with open(filename, encoding="utf-8") as f:
+                code = f.read()
+
+                print(evaluate(code))
 
     typer.run(cli)
