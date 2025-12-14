1803d59df0831cb712e2fba591d96579bd82ef46
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-12-14 11:02:29 -0500

    feat: remove persistent history file
    As this has bugs and I plan on shipping now, I have no choice but to
    remove this.

--- src/pratt_calc/repl.py
+++ src/pratt_calc/repl.py
@@ -1,26 +1,11 @@
-import atexit
 import cmd
-import os
-import readline
 from typing import final, override
 
 from pratt_calc.evaluator import Evaluator
 
 
-def _save(prev_hlen: int, histfile: str):
-    """Helper function to append lines to history."""
-
-    new_hlen = readline.get_current_history_length()
-    readline.set_history_length(1000)
-    readline.append_history_file(new_hlen - prev_hlen, histfile)
-
-
 @final
 class Repl(cmd.Cmd):
-    # FIXME: we should let the user customize where this is stored, as
-    # well as the capacity of the file itself.
-    histfile = os.path.join(os.path.expanduser("~"), ".pratt_calc_history")
-
     intro = """Welcome to the Pratt Calc REPL.
 
 Use Ctrl+D (or the 'exit' command) to exit.
@@ -31,16 +16,6 @@ Type 'help' or '?' to list all commands."""
 
     def __init__(self, ev: Evaluator):
         super().__init__()
-
-        try:
-            readline.read_history_file(self.histfile)
-            history_length = readline.get_current_history_length()
-        except FileNotFoundError:
-            # Create the file for the first time.
-            open(self.histfile, "wb").close()
-            history_length = 0
-
-        _ = atexit.register(_save, history_length, self.histfile)
 
         # Initialize the evaluator to be shared across prompts.
         self.ev = ev
